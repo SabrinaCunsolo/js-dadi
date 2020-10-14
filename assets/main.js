8d96da23bc2623d3162a661d2571234bd86a228e
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-14 23:36:08 +0200

    parte2

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,6 @@
 // Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 
-
 // Genero un numero casuale tra 1 e 6 "lancio1"
 document.getElementById("lancio1").innerHTML =
 Math.floor(Math.random() * 6) + 1;
@@ -11,3 +10,14 @@ Math.floor(Math.random() * 6) + 1;
 // Genero un numero casuale tra 1 e 6 "lancio2"
 document.getElementById("lancio2").innerHTML =
 Math.floor(Math.random() * 6) + 1;
+
+
+// Adesso devo comunicare chi ha vinto e chi non ha vinto e se pareggiano
+
+if (lancio1 > lancio2) {
+    console.log("Vince il primo giocatore");
+} else if (lancio1 == lancio2) {
+    console.log("Avete ottenuto un pareggio!");
+} else {
+    console.log("Vince il secondo giocatore");
+}
