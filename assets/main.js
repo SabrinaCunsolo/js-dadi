1b2166f0dea4862ba76385b6aa5894789bc76d56
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-14 16:28:56 +0200

    Gioco dadi parte1

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,12 @@
 // Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
 // Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
+
+
+// Genero un numero casuale tra 1 e 6 "lancio1"
+document.getElementById("lancio1").innerHTML =
+Math.floor(Math.random() * 6) + 1;
+
+// Genero un numero casuale tra 1 e 6 "lancio2"
+document.getElementById("lancio2").innerHTML =
+Math.floor(Math.random() * 6) + 1;
