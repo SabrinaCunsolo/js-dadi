a40fc8ce0ffd82213494158c4aaa83baeb81b301
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-15 14:42:04 +0200

    correzione esercizio

--- assets/main.js
+++ assets/main.js
@@ -4,20 +4,19 @@
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 
 // Genero un numero casuale tra 1 e 6 "lancio1"
-document.getElementById("lancio1").innerHTML =
-Math.floor(Math.random() * 6) + 1;
+var lancio1 = Math.floor(Math.random() * 6) + 1;
+console.log("giocata 1 = " + lancio1);
 
 // Genero un numero casuale tra 1 e 6 "lancio2"
-document.getElementById("lancio2").innerHTML =
-Math.floor(Math.random() * 6) + 1;
-
+var lancio2 = Math.floor(Math.random() * 6) + 1;
+console.log("giocata 2 = " + lancio2);
 
 // Adesso devo comunicare chi ha vinto e chi non ha vinto e se pareggiano
 
 if (lancio1 > lancio2) {
     console.log("Vince il primo giocatore");
-} else if (lancio1 == lancio2) {
-    console.log("Avete ottenuto un pareggio!");
-} else {
+} else if (lancio2 > lancio1) {
     console.log("Vince il secondo giocatore");
+} else {
+    console.log("Avete ottenuto un pareggio!");
 }
